456c6086e195b60acd1471ce22b10b39604f4742
skyforce77 <arnaud.bruyer@aliceadsl.fr>
2014-06-21 12:24:27 +0200

    0.2

--- src/fr/skyforce77/TMCommands/CommandListener.java
+++ src/fr/skyforce77/TMCommands/CommandListener.java
@@ -1,9 +1,17 @@
 package fr.skyforce77.TMCommands;
 
+import java.awt.Color;
+
+import fr.skyforce77.towerminer.TowerMiner;
 import fr.skyforce77.towerminer.api.EventHandler;
 import fr.skyforce77.towerminer.api.TMListener;
+import fr.skyforce77.towerminer.api.events.chat.CommandTypedEvent;
 import fr.skyforce77.towerminer.api.events.menu.MenuUsedEvent;
+import fr.skyforce77.towerminer.menus.MultiPlayer;
 import fr.skyforce77.towerminer.menus.SinglePlayer;
+import fr.skyforce77.towerminer.protocol.chat.ChatMessage;
+import fr.skyforce77.towerminer.protocol.chat.ChatModel;
+import fr.skyforce77.towerminer.protocol.chat.MessageModel;
 
 public class CommandListener extends TMListener{
 	
@@ -13,6 +21,19 @@ public class CommandListener extends TMListener{
 			SinglePlayer sp = ((SinglePlayer)e.getMenu());
 			sp.chatfield.setVisible(true);
 	        sp.enablechat.setVisible(true);
+		}
+	}
+	
+	@EventHandler
+	public void onCommandTyped(CommandTypedEvent e) {
+		if(TowerMiner.menu instanceof MultiPlayer) {
+			MultiPlayer mp = (MultiPlayer)TowerMiner.menu;
+			if(!mp.server) {
+				ChatModel m = new ChatModel("You can't use commands if you are not the host");
+				m.setForegroundColor(Color.RED);
+				m.setMouseModel(new MessageModel("Will be available in a future version"));
+				mp.chat.onMessageReceived(new ChatMessage(m));
+			}
 		}
 	}
 
