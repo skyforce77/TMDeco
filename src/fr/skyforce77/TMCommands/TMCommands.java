7a4ba5107e5d45129d6b163cfa2845c611ffb26d
skyforce77 <arnaud.bruyer@aliceadsl.fr>
2014-06-21 20:02:38 +0200

    plugins command

--- src/fr/skyforce77/TMCommands/TMCommands.java
+++ src/fr/skyforce77/TMCommands/TMCommands.java
@@ -1,6 +1,7 @@
 package fr.skyforce77.TMCommands;
 import fr.skyforce77.TMCommands.commands.CommandMusic;
 import fr.skyforce77.TMCommands.commands.CommandNotif;
+import fr.skyforce77.TMCommands.commands.CommandPlugins;
 import fr.skyforce77.TMCommands.commands.CommandStop;
 import fr.skyforce77.towerminer.api.Plugin;
 import fr.skyforce77.towerminer.api.PluginManager;
@@ -20,6 +21,7 @@ public class TMCommands extends Plugin{
 		CommandManager.register("notif", new CommandNotif());
 		CommandManager.register("stop", new CommandStop());
 		CommandManager.register("music", new CommandMusic());
+		CommandManager.register("plugins", new CommandPlugins());
 	}
 
 }
